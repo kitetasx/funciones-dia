7d071d165333e6b8064a1bb7c478c499f3c25d80
Kite Tasx <kitetasxpete@gmail.com>
2024-10-03 14:52:39 -0500

    se crea el proyecto con el archivo main y una funcion con el hola mundo

---
+++ funcionesProject1/Main.py
@@ -0,0 +1,16 @@
+# Definicion de una funcion
+
+def my_function():
+    print("Hello from a function")
+
+# Invicar la funcion
+my_function()
+
+def mostrarNombre(nombre):
+    print("Su nombre es "+nombre)
+nom = input("Ingrese su nombre: ")
+
+mostrarNombre(nom)
+
+
+
